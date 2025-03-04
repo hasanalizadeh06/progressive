b6d02477ee8a4b8d4be87312c762118368b597fd
HasanAlizadeh <texnovideo181@gmail.com>
2025-03-04 12:28:35 +0400

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 import "./Navbar.css"
+import { NavLink } from 'react-router-dom'
+
+import logo from "../../assets/navbar/logo2.png"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="logo">
+        <NavLink to="">
+          <img src={logo} alt="" />
+        </NavLink>
+      </div>
+      <div className="links">
+        <ul>
+          <li>
+            <NavLink to="about-us">
+              About us
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="services">
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="blog">
+              Blog
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="faq">
+              FAQ
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="contact">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="register">
+        <NavLink to="register">
+          REGISTER NOW
+        </NavLink>
+      </div>
+    </div>
   )
 }
 
--- src/components/router/AppRouter.jsx
+++ src/components/router/AppRouter.jsx
@@ -7,6 +7,13 @@ function AppRouter() {
   return (
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Main/>}/>
+          <Route path="/about-us" element={<Main/>}/>
+          <Route path="/services" element={<Main/>}/>
+          <Route path="/blog" element={<Main/>}/>
+          <Route path="/faq" element={<Main/>}/>
+          <Route path="/contact" element={<Main/>}/>
+          <Route path="/register" element={<Main/>}/>
+          <Route path="*" element={<>404 Not Found</>}/>
         </Routes>
     )
 }
