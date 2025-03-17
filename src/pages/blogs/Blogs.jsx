e7e4b57a9c1f5bcc33f7876f74da20eac7f6c529
HasanAlizadeh <texnovideo181@gmail.com>
2025-03-17 11:54:37 +0400

    about&blogs

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -2,15 +2,68 @@ import React from 'react'
 import './Blogs.css'
 import Rotation from '../../components/rotation/Rotation'
 import { IoArrowForward } from 'react-icons/io5'
+import { RxArrowTopRight } from 'react-icons/rx'
 
 
 function Blogs() {
   return (
-    <div>
+    <div className='blogs-page'>
       <Rotation>
         home <IoArrowForward /> blogs
       </Rotation>      
-      Blogs
+      <div className="header">
+        LOREM IMPUS
+      </div>
+      <div className="content">
+        <div className="psychologists">
+          <div className="psychologist-card">
+            <div className="info">
+              <div className="name">LOREM IPSUM</div>
+              <div className="title">27.02.2025</div>
+              <RxArrowTopRight className='linker'/>
+            </div>
+          </div>
+          <div className="psychologist-card small">
+            <div className="info">
+              <div className="name">LOREM IPSUM</div>
+              <div className="title">27.02.2025</div>
+              <RxArrowTopRight className='linker'/>
+              </div>
+          </div>
+          <div className="psychologist-card">
+            <div className="info">
+              <div className="name">LOREM IPSUM</div>
+              <div className="title">27.02.2025</div>
+              <RxArrowTopRight className='linker'/>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className="content">
+        <div className="psychologists">
+          <div className="psychologist-card small">
+            <div className="info">
+              <div className="name">LOREM IPSUM</div>
+              <div className="title">27.02.2025</div>
+              <RxArrowTopRight className='linker'/>
+            </div>
+          </div>
+          <div className="psychologist-card">
+            <div className="info">
+              <div className="name">LOREM IPSUM</div>
+              <div className="title">27.02.2025</div>
+              <RxArrowTopRight className='linker'/>
+              </div>
+          </div>
+          <div className="psychologist-card small">
+            <div className="info">
+              <div className="name">LOREM IPSUM</div>
+              <div className="title">27.02.2025</div>
+              <RxArrowTopRight className='linker'/>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
